cf22b75601eef8310ccd2f2ca76aeb312661f994
Vincenzo Pace <vincenzo.pace@student.kit.edu>
2021-05-06 11:49:59 +0200

    Solved problem 3

--- 3.hs
+++ 3.hs
@@ -1,1 +1,7 @@
 -- Find the k'th element of a list, first element in the list is number 1
+
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "empty lists are not allowed!"
+elementAt (x:xs) i 
+  | i > 0 = (x:xs) !! (i-1)
+  | otherwise = error "index can only be positive!"
