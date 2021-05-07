71fca6fb35adbd943015fc70091152a6013ad714
Vincenzo Pace <vincenzo.pace@student.kit.edu>
2021-05-07 21:28:56 +0200

    Solved 6

---
+++ 6.hs
@@ -0,0 +1,11 @@
+-- Find out whether a list is a palindrome
+
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome list
+  | head list == last list = isPalindrome $ tail $ init list
+  | otherwise = False
+
+isPalindrome' :: Eq a => [a] -> Bool
+isPalindrome' list = list == reverse list
