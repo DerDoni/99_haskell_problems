143ea722a0e76052636cea9440b2c0ccfd98a45d
Vincenzo Pace <vincenzo.pace@student.kit.edu>
2021-07-19 19:03:58 +0200

    Solved exercise 20 with Maybe

--- 20.hs
+++ 20.hs
@@ -1,1 +1,4 @@
-removeAt :: Int -> [a] -> [a]
+removeAt :: Int -> [a] -> (Maybe a, [a])
+removeAt k list
+  | (k < 0) || (k > length list) = (Nothing, list)
+  | otherwise = (Just (last $ take k list), take (k-1) list ++ drop k list)
